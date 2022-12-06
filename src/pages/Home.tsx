46f5ce9099c7cc430281b477867c52804cc37573
fernando predes <fernando_predes@hotmail.com>
2022-12-06 12:51:03 -0300

    next and previous working

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,22 +18,33 @@ const Home = () => {
 
   const [cards, setCards] = useState<CardProps[]>([])
   const [show, setShow] = useState(10)
+  const [offSet, setOffSet] = useState(0)
   const [total, setTotal] = useState(0)
+  const [totalShow, setTotalShow] = useState(10)
 
   async function getCards() {
-    const { data } = await  api.get(`/?limit=${show}&offset=0`)
+    const { data } = await  api.get(`/?limit=${show}&offset=${offSet}`)
     setCards(data.results)
-    setTotal(data.results.length)
+    setTotal(data.count)
+    console.log(total)
     }
 
     useEffect(() => {
       getCards()
+      console.log(`show ${totalShow} total ${total}`)
+    }, [show, offSet])
 
-    }, [show])
+    function next() {
 
-    function showMore() {
+      setOffSet(offSet+10)
+      setTotalShow(totalShow+10)
+      document.documentElement.scrollTop = 0
+    }
 
-      setShow(show+10)
+    function previous() {
+
+      setOffSet(offSet-10)
+      document.documentElement.scrollTop = 0
 
     }
 
@@ -48,7 +59,8 @@ const Home = () => {
           {cards.map((card)=> (
             <Card username={card.username} created_datetime={card.created_datetime} title={card.title} content={card.content} />
           ))}
-          <button onClick={showMore} disabled={total !== show ? true : false}>show more</button>
+          <button onClick={next} disabled={totalShow >= total ? true : false}>next</button>
+          <button onClick={previous} disabled={offSet <= 0 ? true : false}>previous</button>
       </Page>
     </>
   )
